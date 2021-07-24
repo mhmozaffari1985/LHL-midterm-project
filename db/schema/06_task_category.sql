5f3b004249aed7bd031df9a20d23ccc9ff0052b6
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-07-24 09:32:28 -0400

    change name of table from item_category to task_category

--- db/schema/06_task_category.sql
+++ db/schema/06_task_category.sql
@@ -1,7 +1,7 @@
 -- Drop and recreate Users table (Example)
-DROP TABLE IF EXISTS item_category CASCADE;
+DROP TABLE IF EXISTS task_category CASCADE;
 
-CREATE TABLE item_category (
+CREATE TABLE task_category (
   id SERIAL PRIMARY KEY NOT NULL,
   task_id INTEGER REFERENCES tasks(id) NOT NULL,
   category_id INTEGER REFERENCES categories(id) NOT NULL
---
+++ db/seeds/06_task_category.sql
@@ -0,0 +1,4 @@
+INSERT INTO task_category (task_id, category_id) VALUES (1, 1);
+INSERT INTO task_category (task_id, category_id) VALUES (2, 3);
+INSERT INTO task_category (task_id, category_id) VALUES (3, 5);
+INSERT INTO task_category (task_id, category_id) VALUES (4, 6);
