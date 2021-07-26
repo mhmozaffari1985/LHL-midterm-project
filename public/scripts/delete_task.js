467a0af75cf8a0b608cab00e8d76bb3247e62a4c
Taejin Jung <taejin5314@gmail.com>
2021-07-26 18:15:11 +0000

    add delete function

---
+++ public/scripts/delete_task.js
@@ -0,0 +1,16 @@
+const deleteItem = (id) => {
+  console.log(id);
+
+  if (confirm("Do you really want to delete?")){
+    // if user clicked yes
+    $.ajax({
+      url: `/tasks/${id}`,
+      type: 'POST'
+    }).then(() => {
+      console.log('Successfully delete the item!')
+      location.reload();
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+}
