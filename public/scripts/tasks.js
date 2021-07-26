db11af24a0cc0258d86aca1c8d7366fc557077d3
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-26 02:01:55 +0000

    Add skeleton and test data for renderTasks function in tasks.js. Added a section tag with id 'allTasks' to house each task article.

---
+++ public/scripts/tasks.js
@@ -0,0 +1,105 @@
+// Test Script To Print User Names
+
+$(document).ready(function() {
+
+  // Single Task
+  const task =  {
+    id: 1,
+    task_title: "Inception",
+    task_description: "Very nice movie to watch",
+    deleted: false,
+    user_id: 1,
+    status_id: 1
+  };
+
+  const createTaskElement = function(data) {
+    const $output = $('<article class="tasks">'); // Output is a tasks class article
+
+    // Checkbox
+    const $checkbox = $('<button class="checkbox"></button>');
+
+    // Task Content Container
+    const $taskContent = $('<div class="taskContent">');
+
+    // Task Header Tags
+    const $taskHeader = $('<header class="taskHeader">'); // Set task header tag
+    const $taskTitle = $('<p class="taskTitle">').text(data.task_title); // Set p tag in task header
+    const $deleteTask = $('<button class="deleteTask">').text('Delete Task'); // Set delete button tag
+
+    // Append Header Tags
+    $taskHeader.append($taskTitle).append($deleteTask);
+
+    // Task Body Tags
+    const $taskBody = $('<div class="taskBody">'); // Set div with task body class
+    const $taskDescription = $('<p class="taskDescription">').text(data.task_description); // Set p with taskDescription class
+    const $editButtons = $('<div class="editButtons">') // Set div with editButtons class
+    const $editButton = $('<button>').text('Edit'); // Edit button
+    const $saveButton = $('<button>').text('Save'); // Save button
+
+    // Append Body Tags
+    $editButtons.append($editButton).append($saveButton);
+    $taskBody.append($taskDescription).append($editButtons);
+
+    // Task Footer
+    const $taskFooter = $('<footer class="taskFooter">'); // Set footer tag with taskFooter class
+
+    const $categoryContainer = $('<div class="categoryContainer">') // Set div with class categoryContainer
+
+    // For the real script, there should be a loop here for each category including the append step!
+    const $categories = $('<p class="categories">').text('Some Category');
+
+    $categoryContainer.append($categories)
+
+    const $addCategory = $('<button class="addCategory">').text('Add Category To Item'); // Set addCategory button
+
+    // Append Footer tags
+    $taskFooter.append($categoryContainer).append($addCategory);
+
+    // Append $output
+    $taskContent.append($taskHeader).append($taskBody).append($taskFooter);
+    $output.append($checkbox).append($taskContent);
+
+    return $output;
+  };
+
+  // All tasks
+  const tasks = [
+    {
+    id: 1,
+    task_title: "Inception",
+    task_description: "Very nice movie to watch",
+    deleted: false,
+    user_id: 1,
+    status_id: 1
+    },
+    {
+    id: 2,
+    task_title: "The Butcher Chef",
+    task_description: "Yummy",
+    deleted: false,
+    user_id: 1,
+    status_id: 2
+    },
+    {
+    id: 3,
+    task_title: "Mistborn",
+    task_description: "Good book",
+    deleted: false,
+    user_id: 2,
+    status_id: 2
+    },
+    {
+    id: 4,
+    task_title: "Play Station 5",
+    task_description: "for having fantastic time",
+    deleted: false,
+    user_id: 2,
+    status_id: 1
+    }];
+
+    const renderTasks = function (data) {
+
+    }
+
+
+});
