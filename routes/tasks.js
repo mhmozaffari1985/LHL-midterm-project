c4881d6697a21c0b081dfbe5f5549c7f27928570
Taejin Jung <taejin5314@gmail.com>
2021-07-26 13:49:00 +0000

    POST api/tasks/add router

--- routes/tasks.js
+++ routes/tasks.js
@@ -25,7 +25,25 @@ module.exports = (db) => {
   });
 
   router.post("/add", (req, res) => {
-    res.json(req.body)
-  })
+    let queryString = `
+      INSERT INTO tasks (task_title, task_description, user_id, status_id)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;
+    `
+    let queryParams = [req.body.task_title, req.body.task_desc, 1, 1];
+    console.log(queryString, queryParams);
+
+    db.query(queryString, queryParams)
+      .then(data => {
+        const tasks = data.rows;
+        res.redirect('/');
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
   return router;
 };
