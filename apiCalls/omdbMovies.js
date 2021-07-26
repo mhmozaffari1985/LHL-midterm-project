adaf9e8359244835ff32c5d33a0686edfa8042e8
Afsanul Haque Khan <afsanhk@gmail.com>
2021-07-26 23:34:04 +0000

    Add some categorization logic to omdbMovies.js within isMovie(). Search in API for taskTitle and look for type = movie and exact name match.

--- apiCalls/omdbMovies.js
+++ apiCalls/omdbMovies.js
@@ -1,25 +1,40 @@
 // Require the npm package for omdb-client
 const omdbApi = require('omdb-client');
+const key = '17b20cf';
 
+// Function to categorize based on OMDB API. Maybe need to change to promise?
+const isMovie = function (taskTitle, taskDesc) {
+  taskTitle = taskTitle.toLowerCase().trim();
+  taskDesc = taskDesc.toLowerCase().trim();
 
-const params = {
-    apiKey: '17b20cf',
-    query: 'Harry Potter'
-}
-
-omdbApi.search(params, function(err, data) {
-
-  console.log(data);
-  if (!err) {
-    for (const movie of data.Search){
-      console.log(movie.Title);
-    }
-  } else {
-    console.log(err);
+  const params = {
+    apiKey: key,
+    query: taskTitle
   }
 
-});
+  omdbApi.search(params, function(err, data) {
 
-const isMovie = function (taskTitle, taskDescription) {
+    if (!err) {
+      for (const movie of data.Search){
+        let type = movie.Type.toLowerCase();
+        let omdbTitle = movie.Title.toLowerCase();
+        if(type === 'movie' && omdbTitle === taskTitle) {
+          console.log(`Movie found: ${movie.Title}`);
+          return true;
+        }
+      }
+      console.log(`No exact match found for: ${taskTitle}`)
+      return false;
+    } else {
+      console.log(err); // If movie is not found, err === Movie not found!
+      return false;
+    }
+
+  });
 
 }
+
+isMovie('Inception','Movie to watch');
+isMovie('asdkjhasdj', 'asdjkhasd');
+isMovie('Harry Potter and the chamber of secrets', 'some test');
+isMovie('Harry Potter', 'askjdhasdkj')
