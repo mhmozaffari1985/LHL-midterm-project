a72704bbe21761fccec22560b2ea8e5cd71d9766
Taejin Jung <taejin5314@gmail.com>
2021-07-27 21:40:06 +0000

    login and logout route

--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,47 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt  = require('bcrypt');
 
 module.exports = (db) => {
-  // render login page
+  // GET /users/login -> render login page
   router.get('/login', (req, res) => {
     res.render('login');
   });
 
+  // POST /users/login -> create logged in user cookie
+  router.post('/login', (req, res) => {
+    // Extract relevant data
+    const { email, password } = req.body;
+
+    let queryString = `
+      SELECT * FROM users
+      WHERE email = $1
+      AND password = $2;
+    `
+    let queryParams = [email, password];
+
+    console.log(queryString, queryParams)
+    db.query(queryString, queryParams)
+      .then((data) => {
+        if (data.rows.length) {
+          req.session.userID = data.rows.id;
+          res.redirect('/');
+        } else {
+          res.send('<script>alert("Your credential is not valid!"); window.location.href = "/users/login";</script>')
+        }
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      })
+  })
+
+  // POST /users/logout -> clear the cookie session
+  router.post('/logout', (req, res) => {
+    req.session = null;
+    res.redirect('/users/login');
+  })
   return router;
 };
