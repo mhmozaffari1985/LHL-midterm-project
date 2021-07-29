a4f8c94e5baa6c355fec9cb432d00f0eac9ca7db
Taejin Jung <taejin5314@gmail.com>
2021-07-29 16:08:13 +0000

    edit task button will cover both description and title

--- public/scripts/edit_task.js
+++ public/scripts/edit_task.js
@@ -1,94 +1,128 @@
 // Edit task button => showing editTextArea and save button
 const editTask = (element, id) => {
   const $taskBody = $(element).parents()[1];
+  const $taskHeader = $($(element).parents()[2]).children()[0];
+
   const $originalDesc = $($taskBody).children()[0];
-  const originalDesc = $($($taskBody).children()[0]).text();
+  const $originalTitle = $($taskHeader).children()[0];
+
+  const originalDesc = $($originalDesc).text();
+  const originalTitle = $($originalTitle).text();
+
   $($originalDesc).remove();
+  $($originalTitle).remove();
 
   const $editTextarea = $(`<textarea class="editTextarea"></textarea>`);
+  const $editTitle = (`<textarea class="editTaskTitle"></textarea>`)
+
   $($taskBody).prepend($editTextarea);
+  $($taskHeader).prepend($editTitle);
+
   // finding editTextarea from the edit button(parent, children)
   $editButtons = $(element).parent();
-  const $saveButton = $(`<button class="btn btn-outline-secondary" onClick="saveTask(this, ${id}, '${originalDesc}')">`).html('<i class="far fa-save"></i>'); // Save button
-  const $textarea = $($(element).parents()[1]).children()[0];
+  const $saveButton = $(`<button class="btn btn-outline-secondary" onClick="saveTask(this, ${id}, '${originalDesc}', '${originalTitle}')">`).html('<i class="far fa-save"></i>'); // Save button
+  const $textareaDesc = $($taskBody).children()[0];
+  const $textareaTitle = $($taskHeader).children()[0];
+
   // remove edit button
   $(element).remove();
   // add save button
   $($editButtons).append($saveButton);
 
   // textarea focus when edit button is clicked
-  $($textarea).focus().val(originalDesc);
+  $($textareaDesc).focus().val(originalDesc);
+  $($textareaTitle).focus().val(originalTitle);
 }
 
-// Edit Title button => showing edit title textarea and save button
-const editTitle = (element, id) => {
-  // remove task title and edit button
-  $taskHeader = $(element).parents()[1];
-  const originalTitle = $($($taskHeader).children()[0]).text();
-  $($taskHeader).children()[0].remove();
-  $(element).remove();
+// // Edit Title button => showing edit title textarea and save button
+// const editTitle = (element, id) => {
+//   // remove task title and edit button
+//   $taskHeader = $(element).parents()[1];
+//   const originalTitle = $($($taskHeader).children()[0]).text();
+//   $($taskHeader).children()[0].remove();
+//   $(element).remove();
 
-  $saveTitle = $(`<button class="saveTitle"><i class="far fa-save"></i></button>`)
-  $($taskHeader).prepend($saveTitle);
-  $newTitle = (`<textarea class="editTaskTitle"></textarea>`)
-  $($taskHeader).prepend($newTitle);
+//   $saveTitle = $(`<button class="saveTitle"><i class="far fa-save"></i></button>`)
+//   $($taskHeader).prepend($saveTitle);
+//   $newTitle = (`<textarea class="editTaskTitle"></textarea>`)
+//   $($taskHeader).prepend($newTitle);
 
-  const $textarea = $($taskHeader).children()[0];
-  // focus on the textarea if the edit button is clicked
-  $($textarea).focus().val(originalTitle);
+//   const $textarea = $($taskHeader).children()[0];
+//   // focus on the textarea if the edit button is clicked
+//   $($textarea).focus().val(originalTitle);
 
-  // in textarea: enter => no line break
-  $($textarea).keypress(function(event) {
-    if (event.keyCode == 13) {
-        event.preventDefault();
-    }
-  });
+//   // in textarea: enter => no line break
+//   $($textarea).keypress(function(event) {
+//     if (event.keyCode == 13) {
+//         event.preventDefault();
+//     }
+//   });
 
-  // when the save button is clicked
-  $($($taskHeader).children()[1]).on('click', () => {
-    const edittedTitle = $($textarea).val();
+//   // when the save button is clicked
+//   $($($taskHeader).children()[1]).on('click', () => {
+//     const edittedTitle = $($textarea).val();
 
-    if (edittedTitle) {
-      $.ajax({
-        url: `/tasks/update/${id}`,
-        type: 'POST',
-        data: { task_title: edittedTitle}
-      }).then(() => {
-        console.log('Successfully update task title');
-        loadTasks();
-      }).catch((err) => {
-        console.log(err);
-      })
-    } else {
-      $.ajax({
-        url: `/tasks/update/${id}`,
-        type: 'POST',
-        data: { task_title: originalTitle}
-      }).then(() => {
-        console.log('Successfully update task title');
-        loadTasks();
-      }).catch((err) => {
-        console.log(err);
-      })
-    }
+//     if (edittedTitle) {
+//       $.ajax({
+//         url: `/tasks/update/${id}`,
+//         type: 'POST',
+//         data: { task_title: edittedTitle}
+//       }).then(() => {
+//         console.log('Successfully update task title');
+//         loadTasks();
+//       }).catch((err) => {
+//         console.log(err);
+//       })
+//     } else {
+//       $.ajax({
+//         url: `/tasks/update/${id}`,
+//         type: 'POST',
+//         data: { task_title: originalTitle}
+//       }).then(() => {
+//         console.log('Successfully update task title');
+//         loadTasks();
+//       }).catch((err) => {
+//         console.log(err);
+//       })
+//     }
 
-  });
-
-}
+//   });
+// }
 
 // Save button => update database
-const saveTask = (element, id, originalDesc) => {
-  const $textarea = $($(element).parents()[1]).children()[0];
-  const edittedText = $($textarea).val();
+const saveTask = (element, id, originalDesc, originalTitle) => {
+  const $textareaDesc = $($(element).parents()[1]).children()[0];
+  const $textareaTitle = $($($(element).parents()[2]).children()[0]).children()[0];
+  const edittedDesc = $($textareaDesc).val();
+  const edittedTitle = $($textareaTitle).val();
 
-  if (edittedText) {
+  if (edittedDesc) {
     $.ajax({
       url: `/tasks/update/${id}`,
       type: 'POST',
-      data: { task_description: edittedText }
+      data: { task_description: edittedDesc }
     }).then(() => {
-      console.log('Successfully update task description');
-      loadTasks();
+      if (edittedTitle) {
+        $.ajax({
+          url: `/tasks/update/${id}`,
+          type: 'POST',
+          data: { task_title: edittedTitle }
+        }).then(() => {
+          loadTasks();
+        }).catch((err) => {
+          console.log(err);
+        })
+      } else {
+        $.ajax({
+          url: `/tasks/update/${id}`,
+          type: 'POST',
+          data: { task_title: originalTitle }
+        }).then(() => {
+          loadTasks();
+        }).catch((err) => {
+          console.log(err);
+        })
+      }
     }).catch((err) => {
       console.log(err);
     })
@@ -98,8 +132,27 @@ const saveTask = (element, id, originalDesc) => {
       type: 'POST',
       data: { task_description: originalDesc }
     }).then(() => {
-      console.log('Successfully update task description');
-      loadTasks();
+      if (edittedTitle) {
+        $.ajax({
+          url: `/tasks/update/${id}`,
+          type: 'POST',
+          data: { task_title: edittedTitle }
+        }).then(() => {
+          loadTasks();
+        }).catch((err) => {
+          console.log(err);
+        })
+      } else {
+        $.ajax({
+          url: `/tasks/update/${id}`,
+          type: 'POST',
+          data: { task_title: originalTitle }
+        }).then(() => {
+          loadTasks();
+        }).catch((err) => {
+          console.log(err);
+        })
+      }
     }).catch((err) => {
       console.log(err);
     })
